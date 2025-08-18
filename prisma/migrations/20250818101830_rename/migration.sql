3ac8a92c0a5802f5305bc4395e3c217dad35f7ba
Kaung Khant Kyaw <kcubez21@gmail.com>
2025-08-18 21:17:57 +0700

    pulling data from existing database and migrate diff & resolve

--- prisma/migrations/20250818101830_rename/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `verifyToken` on the `otp` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."otp" RENAME COLUMN "verifyToken" TO "verifyTokens";
--- prisma/migrations/20250818102200_rename_verify_token/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `verifyTokens` on the `otp` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."otp" RENAME COLUMN "verifyTokens" TO "verifyToken";
